9ffe9ec3feb6ecfa523c464d6c58a60f188db4e2
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2024-05-29 05:32:20 +0530

    Update RightSide.tsx

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const RightSide = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-end gap-16 text-textLight">
-      <a href="mailto:noor.jsdivs@gmail.com">
+      <a href="mailto:jai.bairagi01@gmail.com">
         <p className="text-sm rotate-90 w-72 tracking-widest text-textGreen">
         jai.bairagi01@gmail.com
         </p>
